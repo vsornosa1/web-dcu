3254dc29e5e5e5dc81152eef93241f24d52afa4a
vsornosa1 <vsornosarisen@gmail.com>
2023-01-07 14:21:01 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+import Sidebar from './components/Sidebar.component';
+import TransitionRoot from './components/TransitionRoot.component';
+import Dashboard from './components/MainColumn.jsx/Dashboard.component';
+
+
+
+export default function Example() {
+
+  return (
+    <>
+      <div className="min-h-full">
+        {/* TransitionRoot - per a ajustar responsiveness */}
+        <TransitionRoot />
+
+        {/* Static sidebar per a desktop */}
+        <Sidebar />
+
+        {/* Main column */}
+        <Dashboard />
+      </div>
+    </>
+  )
+}
