6c77a40da94d2975d912e6d6bdfa30c053793556
vsornosa1 <vsornosarisen@gmail.com>
2023-01-18 13:50:56 +0100

    Normativa table component

---
+++ src/components/UI/NormativaTable.component.jsx
@@ -0,0 +1,66 @@
+
+const NormativaTable = (props) => {
+  const { normas } = props;
+
+  return (
+    <div className="px-4 sm:px-6 lg:px-8">
+      <div className="mt-3 flex flex-col">
+        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-300">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                      
+                    </th>
+                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                      Nombre
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                      Profesores
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                      Aprobada
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                      Aplica a
+                    </th>
+                    <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                      <span className="sr-only">Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 bg-white">
+                  {normas.map((norma) => (
+                    <tr key={norma.idx}>
+                      <td className="whitespace-nowrap py-4 pl-4  pr-3 text-sm sm:pl-6">
+                        <div className="font-medium text-gray-900"> {norma.idx} </div>
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        <div className="font-medium text-gray-900">{norma.nombre}</div>
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        <span className="flex justify-center">
+                          {norma.nProfesores}
+                        </span>
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{norma.aprobada}</td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        <span className={`inline-flex rounded-full bg-${norma.color}-100 px-2 text-xs font-semibold leading-5 text-${norma.color}-800`}>
+                          {norma.aplica}
+                        </span>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NormativaTable;
